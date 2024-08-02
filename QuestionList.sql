d1fa4dfea222566b47f4a25a7982f84e38046a2b
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-08-03 01:25:56 +0300

    Add initial version of QuestionList.sql, QuizAPI, and quiz-client

---
+++ QuestionList.sql
@@ -0,0 +1,27 @@
+USE [QuizDB]
+GO
+SET IDENTITY_INSERT [dbo].[Questions] ON 
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (1, N'What is the only continent with land in all four hemispheres?', NULL, N'Australia', N'Africa', N'Europe', N'North America', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (2, N'Which river flows through the Grand Canyon?', NULL, N'Alabama River', N'Colorado River', N'Kansas River', N'Florida River', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (3, N'What is the state capital of New York? ', NULL, N'Albany', N'Tirana', N'Sophia', N'Saraevo', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (4, N'How many stars are on the Chinese flag?' NULL, N'17', N'5', N'3', N'14', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (5, N'What is a Beaujolais?', NULL, N'A type of grape', N'A type of red wine', N'A type of cheese', N'A type of white wine', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (6, N'What country drinks the most coffee? 
+', NULL, N'Finland', N'Ukraine', N'Italy', N'Coloumbia', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (7, N'A murder', NULL, N'', N'A flock', N'A herd', N'A gaggle', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (8, N'What city is known as "The Eternal City"?', NULL, N'Kyiv', N'Rome', N'Tatarstan', N'Paris', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (9, N'What artist has the most streams on Spotify?', NULL, N'Lana Del Rey', N'Pharaoh', N' Drake', N'SSIO', 3)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (10, N'What disease commonly spread on pirate ships?', NULL, N'Malyariya', N'Kurwa', N'Scurvy', N'HIV', 3)
+GO
+SET IDENTITY_INSERT [dbo].[Questions] OFF
+GO
